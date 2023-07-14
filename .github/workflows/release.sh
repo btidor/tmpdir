08d70a3461c82024bbb4509c444d6f26b46d68b1
Benjamin Tidor <benjamin@tidor.net>
2023-07-13 20:55:06 -0700

    wip12

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -71,6 +71,6 @@ for file in "*.whl"; do
     gh api --method POST \
         -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        -f "@$file" \
+        -f "\@$file" \
         "/repos/btidor/tmpdir/releases/$RELEASE_TAG/assets?name=$file"
 done
