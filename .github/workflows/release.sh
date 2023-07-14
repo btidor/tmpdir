d9bf56ac0c0d2406a150b8b15917f67e9a013be9
Benjamin Tidor <benjamin@tidor.net>
2023-07-13 20:50:05 -0700

    wip9

---
+++ .github/workflows/release.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+set -eufx -o pipefail
+
+# Find the latest (annotated) tag and check that it's well-formed.
+RELEASE_TAG=$(git describe --abbrev=0)
+if [[ ! "$(git show $RELEASE_TAG)" =~ "tmpdir@$RELEASE_TAG" ]]; then
+    echo "$(git show $RELEASE_TAG)"
+    echo "error: tag is missing repository and version"
+    exit 1
+fi
+
+curl https://github.com/btidor.gpg | gpg --import
+git verify-tag "$RELEASE_TAG"
+
+# Query Cirrus CI to find the build that was triggered by this tag being
+# created. Note that the build triggered by the corresponding commit will *not*
+# work since those artifacts don't contain the correct version number.
+BUILD_QUERY='
+query ($platform: String!, $owner: String!, $repo: String!, $tag: String!) {
+  ownerRepository(platform: $platform, owner: $owner, name: $repo) {
+    builds(branch: $tag) {
+      edges {
+         node {
+          id,
+          tag,
+          senderUserPermissions,
+          status
+        }
+      }
+    }
+  }
+}'
+
+RESPONSE=$(curl https://api.cirrus-ci.com/graphql --data @- <<EOF
+{
+    "query": "$BUILD_QUERY",
+    "variables": {
+        "platform": "github",
+        "owner": "btidor",
+        "repo": "tmpdir",
+        "tag": "$RELEASE_TAG"
+    }
+}
+EOF
+)
+
+EDGES="$(echo $RESPONSE | jq -e '.data.ownerRepository.builds.edges')"
+if [ "$(echo $EDGES | jq -e '.|length')" != "1" ]; then
+    echo "$EDGES"
+    echo "error: multiple builds found for $CURRENT_TAG"
+    exit 1
+fi
+
+BUILD="$(echo $EDGES | jq -e '.[0].node')"
+if [ "$(echo $NODE | jq -e '.tag')" != "$CURRENT_TAG" ]; then
+    echo "$NODE"
+    echo "error: tag did not match $CURRENT_TAG"
+    exit 1
+else if [ "$(echo $NODE | jq -e '.senderUserPermissions')" != "admin" ]; then
+    echo "$NODE"
+    echo "error: build not initiated by an admin"
+    exit 1
+else if [ "$(echo $NODE | jq -e '.status')" != "COMPLETED"]; then
+    echo "$NODE"
+    echo "error: build not in COMPLETED"
+    exit 1
+fi
+
+gh release create "$RELEASE_TAG" --draft --generate-notes --verify-tag
+
+curl --output wheels.zip https://api.cirrus-ci.com/v1/artifact/build/6420014010466304/wheels.zip
+unzip wheels.zip
+cd wheelhouse
+for file in "*.whl"; do
+    gh api --method POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        -f "@$file" \
+        "/repos/btidor/tmpdir/releases/$RELEASE_TAG/assets?name=$file"
+done
